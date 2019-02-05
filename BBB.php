a17e7fccc035455990926dcdcfc88c7ccdbf069e
Jamie Daws <jdelectronics1@gmail.com>
2019-02-04 20:52:54 -0500

    Added some files

---
+++ BBB.php
@@ -0,0 +1,19 @@
+<?php
+$a = 3;
+if ($a < 20) {
+   switch ($a) {
+       case 0:
+           echo "zero";
+           break;
+       case 1:
+           echo "one";
+           break;
+       case 2:
+           echo "two";
+           break;
+       case 3:
+           echo "three";
+           break;
+   }
+};
+?>
