f38d34889532f43bf45d1cb61ff0fda749e085a3
Jamie Daws <jdelectronics1@gmail.com>
2019-03-12 10:47:19 -0400

    Database changes to add easy switch to/from vagrant/CSSIS

--- Prog03/database.php
+++ Prog03/database.php
@@ -1,36 +1,36 @@
 <?php
-class Database
-{
-    private static $dbName = 'customers' ;
-    private static $dbHost = 'localhost' ;
-    private static $dbUsername = 'root';
-    private static $dbUserPassword = '12345678';
+class Database {
+  /* These are for the Vagrant */
+  private static $dbName = 'customers' ;
+  private static $dbHost = 'localhost' ;
+  private static $dbUsername = 'root';
+  private static $dbUserPassword = '12345678';
+  /* These are for Csis server
+  private static $dbName = 'jpdaws355wi19' ;
+  private static $dbHost = '10.8.30.49' ;
+  private static $dbUsername = 'jpdaws355wi19';
+  private static $dbUserPassword = 'grainpaincheeselog'; */
 
-    private static $cont  = null;
+  private static $cont  = null;
 
     public function __construct() {
         die('Init function is not allowed');
     }
 
-    public static function connect()
-    {
+    public static function connect() {
        // One connection through whole application
-       if ( null == self::$cont )
-       {
-        try
-        {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
-        }
-        catch(PDOException $e)
-        {
-          die($e->getMessage());
-        }
+       if ( null == self::$cont ) {
+           try {
+               self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+           }
+           catch(PDOException $e) {
+               die($e->getMessage());
+           }
        }
        return self::$cont;
     }
 
-    public static function disconnect()
-    {
+    public static function disconnect() {
         self::$cont = null;
     }
 }
