68d6908b99f921c78b4f8ab5c3a1cb5906f73f41
Jamie Daws <jdelectronics1@gmail.com>
2019-03-16 14:38:08 -0400

    Almost done

--- Prog03/login.php
+++ Prog03/login.php
@@ -20,15 +20,6 @@ if (!empty($_POST["join"])) {
 		echo "Email must be of the form something@something.somthing";
 		exit();
 	}
-	$subject = 'Email Verification';
-	$message = 'hello';
-	$headers = 'From: webmaster@customersdatabase.com' . "\r\n" .
-    'Reply-To: webmaster@customersdatabase.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-	mail($username, $subject, $message, $headers);
-}
-
-if (!empty($_POST["valid"])) {
 	// if valid data, insert record into table
 	$pdo = Database::connect();
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -38,8 +29,7 @@ if (!empty($_POST["valid"])) {
 	Database::disconnect();
 }
 
-
-if (!empty($_POST) && empty($_POST["join"]) && empty($_POST["valid"])) { // if $_POST filled then process the form
+if (!empty($_POST)) { // if $_POST filled then process the form
 	// initialize $_POST variables
 	$username = $_POST['username']; // username is email address
 	$password = $_POST['password'];
